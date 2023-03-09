4085ee390070038f54917549b8a61c754c02fbe3
yukie soga <sogayukie@gmail.com>
2023-03-09 16:16:34 +0900

    各種修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   namespace :api, format: 'json' do
     namespace :v1 do
-      resources :events
+      resources :events, only: %i[index show create update destroy]
     end
   end
 end
